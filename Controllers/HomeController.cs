e671d0c54dab120a560cc5b7498d3abf0e46c5fe
Wian <wian@mountainhops.ac.za>
2023-07-13 14:07:06 +0200

    Added Autho0 And User UI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SchoolManagementApp.MVC.Models;
 
 namespace SchoolManagementApp.MVC.Controllers;
-
+[AllowAnonymous]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
